4269febd257bcd1c707dccce0463c9d447a39817
Garan Guillory <garanguillory@gmail.com>
2015-03-17 17:09:31 -0500

    Edit css, js, and html files. Hardcoded version.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,35 @@
 $(document).ready(function(){
   
 
+
     $('form').on('submit', function(e){
       e.preventDefault();
       var something = $('#item').val();
+      var quantity = $('#quantity').val();
       console.log(this);
-      $('<li><i class="fa fa-check"></i>' + something + '<i class="fa fa-remove"></i></li>').appendTo('ul');
+      $('<li class="btn-sm btn-primary"><i class="fa fa-check"></i>' + something + ": " + quantity + '<i class="fa fa-remove"></i></li>').appendTo('.groceries');
 
     });
 
-    $('ul').on('click','i.fa.fa-check', function(){
+
+    $('.groceries').on('click','i.fa.fa-check', function(){
       $(this).closest('li').toggleClass('strikethrough');
       console.log(this);
     });
-    $('ul').on('click','i.fa.fa-remove', function(){
+    $('.groceries').on('click','i.fa.fa-remove', function(){
         $(this).closest('li').slideUp();
         console.log(this);
+    });
+
+
+    $('#delete').on('click', function(){
+        console.log("delete button clicked");
+        var sure = prompt("Are you sure you want to delete your list? Y/N").toUpperCase();
+        if (sure == "YES" || sure == "YE" || sure == "Y" || sure == "YESS"){
+          $('li').remove();
+        } else {
+          console.log("okay");
+        }
     });
 
 });
